8ed073c697e4764a73561643db6650a70fb83716
李铭昕 <715557344@qq.com>
2022-07-04 17:25:28 +0800

    增加同步迁移文件的脚本

--- src/Command/SyncMigrationCommand.php
+++ src/Command/SyncMigrationCommand.php
@@ -13,11 +13,14 @@ namespace KY\WorkWxUser\Command;
 
 use Hyperf\Command\Annotation\Command;
 use Hyperf\Command\Command as BaseCommand;
+use Hyperf\Utils\Filesystem\Filesystem;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Finder\Finder;
 
 #[Command]
 class SyncMigrationCommand extends BaseCommand
 {
-    public function __construct()
+    public function __construct(private ContainerInterface $container)
     {
         parent::__construct('sync:work-wx-migrations');
         $this->setDescription('同步所需的迁移文件到项目中');
@@ -25,6 +28,19 @@ class SyncMigrationCommand extends BaseCommand
 
     public function handle()
     {
-        
+        $dir = BASE_PATH . '/vendor/kydev/work-wx-user/migrations';
+
+        $finder = Finder::create()->in($dir)->name('*.php');
+        $fs = $this->container->get(Filesystem::class);
+        $target = BASE_PATH . '/migrations/';
+
+        $fs->makeDirectory($target, force: true);
+
+        foreach ($finder as $item) {
+            $fs->copy(
+                $item->getRealPath(),
+                $target . '/' . $item->getFilename()
+            );
+        }
     }
 }
