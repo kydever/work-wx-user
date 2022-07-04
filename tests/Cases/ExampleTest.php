39fb3d194e9c8a8848c97332210efc5e8b5780fd
李铭昕 <715557344@qq.com>
2022-07-04 18:47:32 +0800

    Format code

--- tests/Cases/ExampleTest.php
+++ tests/Cases/ExampleTest.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of KnowYourself.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @link     https://www.zhiwotansuo.com
+ * @document https://github.com/kydever/work-wx-user/blob/main/README.md
+ * @contact  l@hyperf.io
+ * @license  https://github.com/kydever/work-wx-user/blob/main/LICENSE
  */
 namespace HyperfTest\Cases;
 
